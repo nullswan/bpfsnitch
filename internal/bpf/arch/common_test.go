e29a8b9c55fb6bdc9e3962f7d3096d8b280801bc
nullswan <pro@nullswan.io>
2024-09-12 19:25:32 +0000

    refactor(internal/bpf): update tests for new SyscallToId and IdToSyscall names

--- internal/bpf/arch/common_test.go
+++ internal/bpf/arch/common_test.go
@@ -2,23 +2,23 @@ package bpfarch
 
 import "testing"
 
-func TestWhitelistedSyscalls(t *testing.T) {
-	for name, number := range WhitelistedSyscalls {
-		if mappedName, present := WhitelistedSyscallsMap[number]; !present ||
+func TestSyscallDefs(t *testing.T) {
+	for name, number := range SyscallToId {
+		if mappedName, present := IdToSyscall[number]; !present ||
 			mappedName != name {
 			t.Errorf(
-				"Mismatch or missing entry: syscalls name %s with number %d not found in reverse map",
+				"Mismatch or missing entry: syscall name %s with number %d not found in reverse map",
 				name,
 				number,
 			)
 		}
 	}
 
-	for number, name := range WhitelistedSyscallsMap {
-		if mappedNumber, present := WhitelistedSyscalls[name]; !present ||
+	for number, name := range IdToSyscall {
+		if mappedNumber, present := SyscallToId[name]; !present ||
 			mappedNumber != number {
 			t.Errorf(
-				"Mismatch or missing entry: syscalls number %d with name %s not found in forward map",
+				"Mismatch or missing entry: syscall number %d with name %s not found in forward map",
 				number,
 				name,
 			)
