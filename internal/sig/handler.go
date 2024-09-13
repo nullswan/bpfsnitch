1b028f906791b4d145f601d2d27bf80dd3a7ccb9
nullswan <pro@nullswan.io>
2024-09-13 00:31:13 +0000

    feat(sig): add signal handling setup for graceful shutdown

---
+++ internal/sig/handler.go
@@ -0,0 +1,37 @@
+package sig
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/nullswan/bpfsnitch/internal/bpf"
+)
+
+func SetupHandler(
+	cancel context.CancelFunc,
+	bpfCtx *bpf.KBContext,
+	log *slog.Logger,
+) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-sigChan
+		log.With("signal", sig).Info("Received signal, cancelling context")
+
+		cancel()
+		for _, tp := range bpfCtx.Tps {
+			tp.Close()
+		}
+		for _, kp := range bpfCtx.Kps {
+			kp.Close()
+		}
+
+		bpfCtx.SyscallEventReader.Close()
+		bpfCtx.NetworkEventReader.Close()
+
+		log.Info("Closed event reader")
+	}()
+}
