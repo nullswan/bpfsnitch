f51f26da1afded2c47c8837001a0c9ac38bf8276
nullswan <pro@nullswan.io>
2024-09-13 00:11:16 +0000

    feat(internal/metrics): add network and DNS metrics counters

--- internal/metrics/server.go
+++ internal/metrics/server.go
@@ -11,12 +11,52 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var DNSQueryCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "dns_query_counter",
+		Help: "Number of DNS queries",
+	},
+	[]string{"container"},
+)
+
 var SyscallCounter = prometheus.NewCounterVec(
 	prometheus.CounterOpts{
 		Name: "syscall_counter",
 		Help: "Number of syscalls",
 	},
 	[]string{"syscall", "container"},
+)
+
+var NetworkReceivedBytesCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "network_received_bytes_counter",
+		Help: "Number of bytes received",
+	},
+	[]string{"container", "remote_ip"},
+)
+
+var NetworkReceivedPacketsCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "network_received_packets_counter",
+		Help: "Number of packets received",
+	},
+	[]string{"container", "remote_ip"},
+)
+
+var NetworkSentBytesCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "network_sent_bytes_counter",
+		Help: "Number of bytes sent",
+	},
+	[]string{"container", "remote_ip"},
+)
+
+var NetworkSentPacketsCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "network_sent_packets_counter",
+		Help: "Number of packets sent",
+	},
+	[]string{"container", "remote_ip"},
 )
 
 func StartServer(log *slog.Logger, cancel context.CancelFunc, port uint32) {
@@ -39,4 +79,9 @@ func RegisterMetrics() {
 	))
 
 	prometheus.MustRegister(SyscallCounter)
+	prometheus.MustRegister(DNSQueryCounter)
+	prometheus.MustRegister(NetworkReceivedBytesCounter)
+	prometheus.MustRegister(NetworkReceivedPacketsCounter)
+	prometheus.MustRegister(NetworkSentBytesCounter)
+	prometheus.MustRegister(NetworkSentPacketsCounter)
 }
